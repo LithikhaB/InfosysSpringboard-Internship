6cc335152f0c19b97a3b6cfffa54b868e18e40c3
Lithikha B <161479842+LithikhaB@users.noreply.github.com>
2024-12-27 15:29:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import nltk
 import re
 import string
